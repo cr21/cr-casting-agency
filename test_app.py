171cc540e4a0ac54be1c3af62a1f76136f1634d3
chirag <cr.tagadiya@gmail.com>
2020-05-19 14:55:46 -0400

    STYLE GUIDE test_app.py

--- test_app.py
+++ test_app.py
@@ -1,7 +1,7 @@
 import unittest
 import os
-from app import app,create_app
-from database.models import db, Movie, Actor,setup_db, test_db_drop_and_create_all
+from app import app, create_app
+from database.models import db, Movie, Actor, setup_db, test_db_drop_and_create_all
 import json
 from datetime import date
 import os
@@ -9,9 +9,10 @@ import os
 
 
 database_path = os.environ['TEST_DATABASE_URL']
-casting_assistant= os.environ['casting_assistant']
+casting_assistant = os.environ['casting_assistant']
 casting_director = os.environ['casting_director']
 producer = os.environ['producer']
+
 
 class Casting_TestCase(unittest.TestCase):
     def setUp(self):
@@ -26,104 +27,133 @@ class Casting_TestCase(unittest.TestCase):
         #     print(rule)
 
         self.actor = {
-                        "name":"Kajol",
-                        "age":45,
+                        "name": "Kajol",
+                        "age": 45,
                         "gender": "Female"
                     }
         self.movie = {
-                        "title":"Ramleela1",
+                        "title": "Ramleela1",
                         "release_date": date.today(),
-                        "actor_id":2
+                        "actor_id": 2
                     }
+ 
     def test_get_movies_with_casting_director(self):
-        res = self.app.get('/movies',headers={'Authorization':casting_assistant})
+        res = self.app.get(
+                            '/movies',
+                            headers={'Authorization': casting_assistant}
+                            )
         # print(res)
         data = json.loads(res.data)
         print(data)
-        self.assertEqual(res.status_code,200)
+        self.assertEqual(res.status_code, 200)
         # self.assertEqual(data['total_record'],4)
     
     def test_get_movies_withoutAuth(self):
         res = self.app.get('/movies')
         print(res.data)
-        self.assertEqual(res.status_code,401)
-
-
+        self.assertEqual(res.status_code, 401)
+ 
     def test_get_actors_with_casting_director(self):
-        res = self.app.get('/actors',headers={'Authorization':casting_assistant})
+        res = self.app.get(
+                            '/actors',
+                            headers={'Authorization': casting_assistant}
+                            )
         data = json.loads(res.data)
-        self.assertEqual(res.status_code,200)
+        self.assertEqual(res.status_code, 200)
         # self.assertEqual(data['total_record'],4)
     
     def test_get_actors_withoutAuth(self):
         res = self.app.get('/actors')
         print(res)
-        self.assertEqual(res.status_code,401)
+        self.assertEqual(res.status_code, 401)
 
     def test_create_new_movie(self):
-        res = self.app.post('/movies', json=self.movie, headers={'Authorization':producer})
+        res = self.app.post(
+                            '/movies',
+                            json=self.movie,
+                            headers={'Authorization': producer}
+                            )
         data = json.loads(res.data)
         # print(data.items)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         
     def test_create_new_movie_auth_error(self):
-        res = self.app.post('/movies', json=self.movie, headers={'Authorization':casting_director})
+        res = self.app.post(
+                            '/movies',
+                            json=self.movie,
+                            headers={'Authorization': casting_director}
+                             )
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 401)
         self.assertEqual(data['success'], False)
 
     def test_create_new_Actor(self):
-        res = self.app.post('/actors', json=self.actor, headers={'Authorization':casting_director})
+        res = self.app.post(
+                            '/actors',
+                            json=self.actor,
+                            headers={'Authorization': casting_director}
+                            )
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        
+      
     def test_create_new_actor_auth_error(self):
-        res = self.app.post('/actors', json=self.actor, headers={'Authorization':casting_assistant})
+        res = self.app.post(
+                            '/actors',
+                            json=self.actor,
+                            headers={'Authorization': casting_assistant}
+                            )
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 401)
         self.assertEqual(data['success'], False)
 
-    
     def test_delete_actors(self):
-        res = self.app.delete('/actors/1',headers={'Authorization':casting_director})
-        print(res)
+        res = self.app.delete(
+                            '/actors/1',
+                            headers={'Authorization': casting_director}
+                            )
+        # print(res)
         self.assertEqual(res.status_code, 200)
         
-    
     def test_patch_movie_success(self):
-        res = self.app.patch('/movies/2', 
-                                headers={'Authorization': producer}, 
-                                json = {"title":"XYZ"}
+        res = self.app.patch(
+                            '/movies/2',
+                            headers={'Authorization': producer},
+                            json={"title": "XYZ"}
                             )
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
     
     def test_patch_movie_Auth_error(self):
-        res = self.app.patch('/movies/3', 
-                                headers={'Authorization': casting_assistant}, 
-                                json = {"title":"XYZ"}
+        res = self.app.patch(
+                            '/movies/3', 
+                            headers={'Authorization': casting_assistant},
+                            json={"title": "XYZ"}
                             )
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 401)
         self.assertEqual(data['success'], False)
-    
+        
     # # delete Failure message 404 error
     def test_404_delete_if_actor_does_not_exist(self):
-        res = self.app.delete('/actors/2000',headers={'Authorization':producer})
+        res = self.app.delete(
+                            '/actors/2000',
+                            headers={'Authorization': producer}
+                            )
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
 
     def test_movie_delete_auth_error(self):
-        res = self.app.delete('/movies/2000',headers={'Authorization':casting_director})
+        res = self.app.delete(
+                            '/movies/2000',
+                            headers={'Authorization': casting_director}
+                            )
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 401)
         self.assertEqual(data['success'], False)
-
-
 
 
 if __name__ == '__main__':
